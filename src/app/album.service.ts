938585bcdd40c41989d0e996502af4cb7262e619
Eric Shull and Nick Brown <eric+nick@>
2018-08-21 15:31:18 -0700

    load new package.json dependencies. Load firebase into album.service load and show firebase albums in marketplace.conponent and marketpalce.html

--- src/app/album.service.ts
+++ src/app/album.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Album } from './album.model';
 import { ALBUMS } from './mock-albums';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Injectable()
 export class AlbumService {
 
-  constructor() { }
+  albums: FirebaseListObservable<any[]>;
 
-  getAlbums() {
-    return ALBUMS;
+  constructor(private database: AngularFireDatabase) {
+    this.albums = database.list('albums');
   }
+
+  // Here, we call database (which is what we named our instance of AngularFireDatabase object in the constructor), then call .list to specify we're gathering a list of multiple things (remember, we're expecting a FirebaseListObservable)
+
+  getAlbums(){
+    return this.albums;
+  }
+  // getAlbums() is called in MarketplaceComponent. It uses our AlbumService to retrieve its data. But because getAlbums() now returns a FirebaseListObservable<any[]> object, we need to make sure the album property in MarketplaceComponent matches this data type:
 
   getAlbumById(albumId: number){
     for (var i = 0; i <= ALBUMS.length - 1; i++) {

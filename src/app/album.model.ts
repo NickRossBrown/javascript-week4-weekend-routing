bbca948dee6dfe5ace3deb27ced16f0b0e888327
Eric Shull and Nick Brown <eric+nick@>
2018-08-21 16:27:44 -0700

    add a admin conponent with routing and add a new album to firebase

--- src/app/album.model.ts
+++ src/app/album.model.ts
@@ -1,3 +1,5 @@
 export class Album {
-  constructor (public title: string, public artist: string, public description: string, public id: number) { }
+  constructor (public title: string, public artist: string, public description: string) { }
+
+  // This is because Firebase will now assign each new Album an id automatically. We no longer have to do this in our application. So, we'll remove the id property from the Album model now, too:
 }
--- src/app/album.service.ts
+++ src/app/album.service.ts
@@ -19,15 +19,19 @@ export class AlbumService {
   }
   // getAlbums() is called in MarketplaceComponent. It uses our AlbumService to retrieve its data. But because getAlbums() now returns a FirebaseListObservable<any[]> object, we need to make sure the album property in MarketplaceComponent matches this data type:
 
+
+  addAlbum(newAlbum: Album) {
+    this.albums.push(newAlbum);
+  }
+
+//   Here, the addAlbum() method refers to the this.albums defined in the service's constructor. this.albums refers to the specific area of our database where our list of Albums is stored.
+//
+// Because this.albums is a FirebaseListObservable<any[]>, as declared at the top of the file, it has many of the same properties and capabilities of any other list or array. We can simply call push() on it to add our new album to the list.
+
   getAlbumById(albumId: number){
-    for (var i = 0; i <= ALBUMS.length - 1; i++) {
-      if (ALBUMS[i].id === albumId) {
-        return ALBUMS[i];
-      }
-    }
+
+
+
   }
 
 }
-
-
-// As you can see, it simply returns the list of Albums we imported into the file in the line import { ALBUMS } from './mock-albums';

2ebc3c679eb474ba31934aa1d4c4a5b4b7a93fea
Eric Shull and Nick Brown <nick@>
2018-08-21 18:41:14 -0700

    album-detail subscribes to observables and removes async pipe in html

--- src/app/album-detail/album-detail.component.ts
+++ src/app/album-detail/album-detail.component.ts
@@ -15,7 +15,7 @@ import { FirebaseObjectObservable } from 'angularfire2/database';
 })
 export class AlbumDetailComponent implements OnInit {
   albumId: string;
-  albumToDisplay;
+  albumToDisplay: Album;
 
   constructor(
     private route: ActivatedRoute,
@@ -25,16 +25,35 @@ export class AlbumDetailComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.route.params.forEach((urlParameters) => {
-     this.albumId = urlParameters['id'];
+    this.route.params.forEach((urlParametersArray) => {
+     this.albumId = urlParametersArray['id'];
    });
-   this.albumToDisplay = this.albumService.getAlbumById(this.albumId);
+   this.albumService.getAlbumById(this.albumId).subscribe(dataLastEmittedFromObserver => {
+     this.albumToDisplay = new Album(dataLastEmittedFromObserver.title,
+                                      dataLastEmittedFromObserver.artist,
+                                      dataLastEmittedFromObserver.description)
+   })
   }
-//  --- We import the FirebaseObjectObservable package at the top of the file.
-// ---We've update the albumId property to be a string here, too.
-// ---albumToDisplay is no longer an Album object.
-// ---We remove the parseInt() from ngOnInit(), because Firebase is now expecting a string key.
+// --Here, we've added subscribe() to the same area of code that printed a FirebaseObjectObservable to our console at the beginning of the lesson.
+//
+//-this.database.object('/items/' + albumId) will result in a FirebaseObjectObservable, as we saw a moment ago.
+//
+//-We call subscribe() upon the FirebaseObjectObservable. Note that this method can only be called upon Observable objects.
+//
+//-Also, notice subscribe() uses the fat arrow notation we discussed previously, in the Dynamic Routing lesson.
+//
+//-The dataLastEmittedFromObserve variable in subscribe()'s parameters represents the data passed from Firebase using the FirebaseObjectObservable. Remember, the observer itself is not the data being provided by Firebase. Instead, it's the pipeline by which that data enters our application.
 
+
+
+
+
+
+// ---Essentially, if you need to do something to a Firebase entry, but you can't access its properties or call class-specific methods on it because it's an Observable, consider using subscribe().
+//
+//---Or, if you want to use your own custom pipe in a template, consider subscribing to your data manually instead of relying on async. This will allow you to apply your own pipe in the template instead.
+//
+//---Lastly, know that observables aren't exclusive to Angular or Firebase, either! They're actually a new proposed standard for managing asynchronous data, and will be included in the ES7 version of JavaScript. There are already JavaScript libraries like RxJS that allow developers to utilize observers in almost any type of project, in any JavaScript framework.
 
 
 

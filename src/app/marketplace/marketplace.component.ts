598dfe331691a694ee312ed03a3e923c592b96f9
Eric Shull and Nick Brown <eric@>
2018-08-21 17:20:28 -0700

    call edit-album route in marketplace conpmonent with two way binding(not yet functional)

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -13,12 +13,15 @@ import { FirebaseListObservable } from 'angularfire2/database';
 export class MarketplaceComponent implements OnInit {
 
   albums: FirebaseListObservable<any[]>;
+  currentRoute: string = this.router.url;
+   // we can use this.router.url to determine which route a component is being rendered in. We can use this information to dynamically hide or show content relevant to that route.
 
   constructor(private router: Router, private albumService: AlbumService){}
   // Just like the Router already present, this ensures all new instances of MarketplaceComponent also have an instance of AlbumService, accessible by calling this.albumService anywhere in the MarketplaceComponent class.
 
   ngOnInit(){
     this.albums = this.albumService.getAlbums();
+    console.log(this.router.url);
   }
 
   goToDetailPage(clickedAlbum) {
